9317d19e0d54b9aae304b877891332d1c929654c
prakash <10prakashgautam@gmail.com>
2025-02-05 00:27:55 -0500

    lab2b.py

---
+++ lab2b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+name = input("Name: ")  # Prompt user for name
+age = input("Age: ")    # Prompt user for age
+
+print("Hi " + name + ", you are " + age + " years old.")  # Print the output correctly
