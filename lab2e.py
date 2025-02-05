77206efc4ba8998d96e5612c3f4b094e465f8152
prakash <10prakashgautam@gmail.com>
2025-02-05 00:29:06 -0500

    lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Prakash Gautam
+# Author ID: Your_seneca_id
+# Date Created: 2025/02/04
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
